26fd75d84724494a4d6d8f2da3a486ee66c8c684
Jeff Pottruff <jeffpottruff@gmail.com>
2020-07-23 13:36:20 -0400

    ADD validation form

--- src/app/components/valid-form/valid-form.component.ts
+++ src/app/components/valid-form/valid-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-valid-form',
@@ -6,10 +7,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./valid-form.component.css']
 })
 export class ValidFormComponent implements OnInit {
+  myForm: FormGroup;
 
-  constructor() { }
+  constructor( private fb: FormBuilder) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    // NOTE: using buildt in 'synchronous' validators
+    // You could pass in asynchronous validators after the Validators array
+
+    this.myForm = this.fb.group({
+      email: ['', [
+            Validators.required,
+            Validators.email
+      ]],
+      password: ['',[
+        Validators.required,
+        // At least one letter and one number
+        Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$')
+      ]],
+      age: [null, [
+        Validators.required,
+        Validators.min(18),
+        Validators.max(65)
+      ]],
+      agree: [false, [
+        Validators.requiredTrue,
+      ]]
+    });
   }
 
+  // Getters
+
+  get email() {
+    return this.myForm.get('email');
+  }
+
+  get password() {
+    return this.myForm.get('password');
+  }
+
+  get age() {
+    return this.myForm.get('age');
+  }
+
+  get agree() {
+    return this.myForm.get('agree');
+  }
 }
