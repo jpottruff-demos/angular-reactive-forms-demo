708b4e1fbae36289e27120e6c82accb78c31e3ed
Jeff Pottruff <jeffpottruff@gmail.com>
2020-08-02 10:58:09 -0400

    ADD submit form. FIXME ngSubmit in form

--- src/app/components/submit-form/submit-form.component.ts
+++ src/app/components/submit-form/submit-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {tap, first } from 'rxjs/operators'
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-submit-form',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubmitFormComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup;
+
+  // Form State
+  loading = false;
+  success = false;
+
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+    this.myForm = this.fb.group({
+      email: ['', Validators.required],
+      message: ['', Validators.required]
+    })
   }
 
+  async submitHandler() {
+    // Setting loading state to true
+    this.loading = true;
+
+    const formValue = this.myForm.value;
+
+    try {
+      await this.mimickDBCall(formValue);
+      this.success = true;
+    } catch(err) {
+      console.log(err);
+    }
+
+    // Set loading back to false
+    this.loading = false;
+  }
+
+  mimickDBCall(formValues) {
+    console.log(formValues);
+
+    //wait 2 seconds then return
+    
+    const dbCallObservable = of('i did it');
+    return Promise.resolve(dbCallObservable);
+  }
 }
